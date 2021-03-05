47184315500883d7338f2a5431712ffb4dda0689
Matt Carvalho <mxc8915@rit.edu>
2021-03-05 10:29:23 -0500

    reword comments

--- flask_app/ytm_api/ytm_client.py
+++ flask_app/ytm_api/ytm_client.py
@@ -6,12 +6,15 @@ from ytmusicapi import YTMusic
 ytmusic: YTMusic = None
 
 """
-Steps for authentication
+Documentation:
+https://ytmusicapi.readthedocs.io/en/latest/setup.html
+
+Steps for initial setup
 open music.youtube.com
 Press f12, open network tab
 Click on the search button in YTM
 Look at the POST request
-Copy Cookie and X-Goog-Visitor-Id to headers_auth.json
+Copy all request headers starting from "accept:" and past this into ./raw_headers.txt
 """
 auth_filepath = os.path.expanduser("~/python/ytm_playlist_manager/flask_app/ytm_api/headers_auth.json")
 raw_header_filepath = os.path.expanduser("~/python/ytm_playlist_manager/flask_app/ytm_api/raw_headers.txt")
@@ -29,8 +32,8 @@ def getYTMClient():
 
 def setupYTMClient():
     """
-    In order to do the initial setup for the YTM client I need to sign in to music.youtube.com, open dev tools
-    and copy the headers that are sent in a POST request to music.youtube.com
+    Does the initial setup of the YTM client.
+    A json file at auth_filepath will be created in this process
     :return:
     """
     global ytmusic
