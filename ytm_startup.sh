81b456350da92808e25fbeae0c31b98440d888f3
Matt Carvalho <mxc8915@rit.edu>
2021-03-15 11:29:12 -0400

    shell sciprt

--- ytm_startup.sh
+++ ytm_startup.sh
@@ -2,9 +2,16 @@
 
 cd ~/python/ytm_playlist_manager/ || exit;
 
+echo "PULLING FROM GIT"
 git pull origin main;
 
+echo "CREATING DB TABLES"
 psql -d ytm < ~/python/ytm_playlist_manager/sql/create_tables.sql
 
+echo "KILLING TMUX"
+tmux kill-session -t react_ytm
+tmux kill-session -t flask_ytm
+
+echo "STARTING TMUX"
 tmux new -s react_ytm -d "cd ~/python/ytm_playlist_manager/frontend_ytm; yarn start"
 tmux new -s flask_ytm -d "cd ~/python/ytm_playlist_manager/; python3 flask_app/flask_app.py"
