c20a30695b525af552e066333447ab19496ac0ed
Matt Carvalho <mxc8915@rit.edu>
2021-03-17 16:35:57 -0400

    fix thumbnails

--- frontend_ytm/src/js/Thumbnail.js
+++ frontend_ytm/src/js/Thumbnail.js
@@ -5,7 +5,7 @@ export default function Thumbnail(props) {
     let src = null;
     if (props.thumbnail) {
         if (props.thumbnail.filepath) {
-            src = `http://nuc:5050/images/${props.thumbnail.filepath}`
+            src = `http://nuc:3000/images/${props.thumbnail.filepath}`
         } else {
             src = props.thumbnail.url
         }
