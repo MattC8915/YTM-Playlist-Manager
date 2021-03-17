c373795bea5cb735f9694ca405597ad712b4b756
Matt Carvalho <mxc8915@rit.edu>
2021-03-17 16:25:56 -0400

    startup script

--- ytm_startup.sh
+++ ytm_startup.sh
@@ -14,4 +14,4 @@ tmux kill-session -t flask_ytm
 
 echo "STARTING TMUX"
 tmux new -s react_ytm -d "cd ~/python/ytm_playlist_manager/frontend_ytm; yarn start"
-tmux new -s flask_ytm -d "cd ~/python/ytm_playlist_manager/; python3 flask_app/flask_app.py"
+tmux new -s flask_ytm -d "cd ~/python/ytm_playlist_manager/flask_app; python3 flask_app.py"
