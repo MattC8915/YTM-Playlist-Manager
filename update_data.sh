caeb4327b21e196340ba80d2ef71f594c25dff89
Matt Carvalho <mxc8915@rit.edu>
2021-04-16 11:22:16 -0400

    update_data bash

--- update_data.sh
+++ update_data.sh
@@ -1,3 +1,4 @@
+cd /home/matt/python/playlist_manager/flask_app/
 export PYTHONPATH='/home/matt/python/playlist_manager/flask_app/:/home/matt/python/playlist_manager/flask_app/cache:/home/matt/python/playlist_manager/flask_app/db:/home/matt/python/playlist_manager/flask_app/ytm_api'; 
 export PYTHONPATH='$PYTHONPATH:/home/matt/java/mhub4/gather/'; 
 echo $PYTHONPATH; 
