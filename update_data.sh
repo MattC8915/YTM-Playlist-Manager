a1ad92a70f5068920aa63a80d8656501183ac5a7
Matt Carvalho <mxc8915@rit.edu>
2021-04-16 11:21:09 -0400

    pythonpath

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,4 @@
-# export PYTHONPATH='/home/matt/python/playlist_manager/flask_app/:/home/matt/python/playlist_manager/flask_app/cache:/home/matt/python/playlist_manager/flask_app/db:/home/matt/python/playlist_manager/flask_app/ytm_api'; 
-export PYTHONPATH='/home/matt/python/playlist_manager/flask_app/'; 
+export PYTHONPATH='/home/matt/python/playlist_manager/flask_app/:/home/matt/python/playlist_manager/flask_app/cache:/home/matt/python/playlist_manager/flask_app/db:/home/matt/python/playlist_manager/flask_app/ytm_api'; 
 export PYTHONPATH='$PYTHONPATH:/home/matt/java/mhub4/gather/'; 
 echo $PYTHONPATH; 
 python3 /home/matt/python/playlist_manager/flask_app/cache/update_cache.py
